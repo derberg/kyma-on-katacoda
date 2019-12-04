72e83d42b784af491bfbb1953ce99942a62beeb7
Lukasz Gornicki <derberg@wp.pl>
2019-12-04 20:11:52 +0100

    fix script

--- katacoda-testing/my-script.sh
+++ katacoda-testing/my-script.sh
@@ -3,35 +3,29 @@ curl -LO https://get.helm.sh/helm-"v2.16.0"-linux-amd64.tar.gz
 tar -xzvf helm-"v2.16.0"-linux-amd64.tar.gz
 mv ./linux-amd64/{helm,tiller} /usr/local/bin
 
-function waitForMinikubeToBeUp() {
-    set +o errexit
+echo "Waiting for minikube to be up..."
 
-    log "Waiting for minikube to be up..." green
+LIMIT=30
+COUNTER=0
 
-	LIMIT=30
-    COUNTER=0
+while [ ${COUNTER} -lt ${LIMIT} ] && [ -z "$STATUS" ]; do
+  (( COUNTER++ ))
+  echo "Keep calm, there are $LIMIT possibilities and so far it is attempt number $COUNTER"
+  STATUS="$(kubectl get pod -n kube-system | grep kube-apiserver-minikube || :)"
+  sleep 1
+done
 
-    while [ ${COUNTER} -lt ${LIMIT} ] && [ -z "$STATUS" ]; do
-      (( COUNTER++ ))
-      log "Keep calm, there are $LIMIT possibilities and so far it is attempt number $COUNTER" green
-      STATUS="$(kubectl get namespaces || :)"
-      sleep 1
-    done
+# In case apiserver is not available get minikube logs
+if [[ -z "$STATUS" ]]; then
+  exit 1
+fi
 
-    # In case apiserver is not available get minikube logs
-    if [[ -z "$STATUS" ]]; then
-      cat /var/lib/minikube/minikube.err
-    fi
-
-    set -o errexit
-
-    log "Minikube is up" green
-}
-
-waitForMinikubeToBeUp
+echo "Minikube is up"
 
 kubectl -n kube-system create serviceaccount tiller
 kubectl create clusterrolebinding tiller --clusterrole cluster-admin --serviceaccount=kube-system:tiller
 helm init --service-account=tiller
+
+clear 
 
 kubectl get deploy -n kube-system | grep tiller-deploy
