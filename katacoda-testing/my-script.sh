bc81386de080def9f70ace793af78aa1ee7b32d8
Lukasz Gornicki <derberg@wp.pl>
2019-12-04 20:18:55 +0100

    delay sleep

--- katacoda-testing/my-script.sh
+++ katacoda-testing/my-script.sh
@@ -1,10 +1,9 @@
-#echo "Upgrade to Helm 1.16.0"
+echo "Upgrade to Helm 1.16.0"
 curl -LO https://get.helm.sh/helm-"v2.16.0"-linux-amd64.tar.gz
 tar -xzvf helm-"v2.16.0"-linux-amd64.tar.gz
 mv ./linux-amd64/{helm,tiller} /usr/local/bin
 
 echo "Waiting for minikube to be up..."
-
 LIMIT=30
 COUNTER=0
 
@@ -12,7 +11,7 @@ while [ ${COUNTER} -lt ${LIMIT} ] && [ -z "$STATUS" ]; do
   (( COUNTER++ ))
   echo "Keep calm, there are $LIMIT possibilities and so far it is attempt number $COUNTER"
   STATUS="$(kubectl get pod -n kube-system | grep kube-apiserver-minikube || :)"
-  sleep 1
+  sleep 3
 done
 
 # In case apiserver is not available get minikube logs
@@ -22,6 +21,7 @@ fi
 
 echo "Minikube is up"
 
+echo "Tiller instalation"
 kubectl -n kube-system create serviceaccount tiller
 kubectl create clusterrolebinding tiller --clusterrole cluster-admin --serviceaccount=kube-system:tiller
 helm init --service-account=tiller
